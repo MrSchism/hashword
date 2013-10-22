dcf0dc2d692f1180a464f592c9cafefe752081a4
Joshua Embrey <mrschism@sdf.org>
2013-10-21 20:27:54 -0400

    Fixed ruby files
    Fixed hashword.rb and included hashword-checker.rb

--- hashword-ruby/hashword.rb
+++ hashword-ruby/hashword.rb
@@ -1,35 +1,36 @@
 #! /usr/bin/env ruby
 
 #####################################################################################
-# hashword-ruby written by:                                                                 
-# 	Joshua "MrSchism" Embrey [mrschism@sdf.org]                                        
-# 	Joseph "Arcarna" Preston [jpreston86@gmail.com]                                        
-# Intial commit: October 16, 2013                                                         
-# Current version: October 16, 2013                                                        
+# hashword-ruby written by:
+# 	Joshua "MrSchism" Embrey [mrschism@sdf.org]
+# 	Joseph "Arcarna" Preston [jpreston86@gmail.com]
+# Intial commit: October 16, 2013
+# Current version: October 19, 2013
 #####################################################################################
 
-# Declare required components
+## Declare required components
 require 'digest'
 
+## Seed input
 puts "\n" # Formatting away from initial line
 puts "Seed?" # Requests starting seed
 system ("stty -echo") # Mitigates shoulder-surfing
 seed = gets.chomp # Input prompt for starting seed
 system ("stty echo") # Re-enables text echo
 
+## Long mode boolean input
 puts "\n" # Formatting
-puts "Use long mode?" # 
-input = gets.chomp
-input = input.downcase
+puts "Use long mode?" # Queries for use of long mode
+input = gets.chomp # A chomped gets for input regarding long mode
+input = input.downcase # Reduces input to downcase
 
-
-if input == "yes"
-	long = Digest::SHA512.hexdigest(seed)
-	puts "\n", "Your hashword is:", "\n", long, "\n"
-elsif input == "no"
-	short = Digest::SHA256.hexdigest(seed)
-	puts "\n", "Your hashword is:", "\n", short, "\n"
-else
-	puts "Please answer Yes or No"
-
-end
+## 'If' statement for presenting long or short hashword
+if input == "yes" # Begins 'if yes' of boolean
+	long = Digest::SHA512.hexdigest(seed) # Defines 'long'
+	puts "\n", "Your hashword is:", "\#" + long, "\n" # Prints defined long hashword
+elsif input == "no" # Begins 'if no' of boolean
+	short = Digest::SHA256.hexdigest(seed) # Defines 'short'
+	puts "\n", "Your hashword is:", "\#" + short, "\n" # Prints defined short hashword
+else # Defines escape if 'input' is neither 'yes' or 'no'
+	puts "Please answer Yes or No" # Suggests trying again and answering 'yes' or 'no'
+end # Ends 'if' statement (and program).
