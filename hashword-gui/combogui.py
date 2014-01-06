3544a79733111f8812e89be0c246f896a130eda7
Joshua Embrey <mrschism@sdf.org>
2014-01-06 00:50:03 -0500

    Fixed enter button support
    Added a style to self.field to allow for enter-button acceptance.

--- hashword-gui/combogui.py
+++ hashword-gui/combogui.py
@@ -5,7 +5,7 @@
 #   Joshua "MrSchism" Embrey [mrschism@sdf.org]					
 #   Joseph "Arcarna" Preston [jpreston86@gmail.com]					
 # Intial commit: December 2, 2013 							
-# Current version: December 4, 2013							
+# Current version: January 6, 22014							
 ######################################################################################
 
 # Declare imports
@@ -15,13 +15,13 @@ import hashlib # Import hashlib for sha2 functionality.
 # Create Seed Dialog box
 class SeedDialog(wx.Dialog):
     def __init__(self, parent, id=-1, title="hashword"):
-        wx.Dialog.__init__(self, parent, id, title, size=(-1, 200))
+        wx.Dialog.__init__(self, parent, id, title, size=(-1, 235))
 
         self.mainSizer = wx.BoxSizer(wx.VERTICAL) # define mainSizer BoxSizer element
         self.buttonSizer = wx.BoxSizer(wx.HORIZONTAL) # Define buttonSizer BoxSizer element
-        self.label = wx.StaticText(self, label="Hashword generator v 1.2 (build 20131207)\nPlease enter your hashword seed.\n\n*NOTE: hashword seeds are case sensitive.*") # Define label StaticText element
+        self.label = wx.StaticText(self, label="Hashword generator v 1.2 (build 201410106)\nPlease enter your hashword seed.\n\n*NOTE: hashword seeds are case sensitive.*") # Define label StaticText element
         self.label2 = wx.StaticText(self, label="Hashword Seed:") # Define label2 StaticText element
-        self.field = wx.TextCtrl(self, value="", size=(300, 20)) # Define field TextCtrl element
+        self.field = wx.TextCtrl(self, value="", size=(300, 20), style=wx.TE_PROCESS_ENTER) # Define field TextCtrl element
         self.okbutton = wx.Button(self, label="Generate", id=wx.ID_OK) # Define okbutton Button element
 
         self.mainSizer.Add(self.label, 0, wx.ALL, 8 ) # Add Label to mainSizer
@@ -30,7 +30,7 @@ class SeedDialog(wx.Dialog):
 
         self.buttonSizer.Add(self.okbutton, 0, wx.ALL, 8 ) # Add okbutton to buttonSizer
 
-        self.mainSizer.Add(self.buttonSizer, 0, wx.ALL, 0) # Add buttonSizer to mainSizer
+        self.mainSizer.Add(self.buttonSizer, 0, wx.ALL, 8) # Add buttonSizer to mainSizer
 
         self.Bind(wx.EVT_BUTTON, self.onOK, id=wx.ID_OK) # Bind "OK" function to ID_OK (ok button)
         self.Bind(wx.EVT_TEXT_ENTER, self.onOK) # Bind OK function to enter key
@@ -50,7 +50,7 @@ class SeedDialog(wx.Dialog):
 # Create initial frame
 class Frame(wx.Frame):
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(320, 100))
+        wx.Frame.__init__(self, parent, title=title, size=(320, 120))
         
         self.panel = wx.Panel(self) # Define panel as a panel element
         self.Bind(wx.EVT_CLOSE, self.OnCloseWindow) # Bind close to close window 
@@ -58,7 +58,7 @@ class Frame(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.GetShort, self.btnShort) # bind opening GetShort to btnShort
         self.btnLong = wx.Button(self.panel, -1, "Long Hashword") # define btnLong as a button element
         self.Bind(wx.EVT_BUTTON, self.GetLong, self.btnLong) # bind opening GetLong to btnLong
-        self.txt = wx.TextCtrl(self.panel, -1, size=(300, -1)) # define txt as a TxtCtrl element
+        self.txt = wx.TextCtrl(self.panel, -1, size=(300, 20)) # define txt as a TxtCtrl element
         self.txt.SetValue('') # set txt's value to blank
 		
         sizer = wx.BoxSizer(wx.VERTICAL) # define sizer as a BoxSizer element
