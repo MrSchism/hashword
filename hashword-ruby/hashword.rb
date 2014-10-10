d3156fbff197b17a61b2afe14d419c4e602756a3
Joshua Embrey <mrschism@sdf.org>
2014-10-10 02:20:34 -0400

    Updating to shorter passwords
    Went to SHA256 for long/MD5 for short because SHA512 was pretty much twice the maximum length of the longest password we found to be accepted.

--- hashword-ruby/hashword.rb
+++ hashword-ruby/hashword.rb
@@ -5,7 +5,7 @@
 # 	Joshua "MrSchism" Embrey [mrschism@sdf.org]
 # 	Joseph "Arcarna" Preston [jpreston86@gmail.com]
 # Intial commit: October 16, 2013
-# Current version: November 26, 2013
+# Current version: October 10, 2014
 #####################################################################################
 
 ## Declare required components
@@ -15,7 +15,7 @@ require 'rbconfig'
 ## Seed input
 system ("clear") # Starts fresh
 puts "\n" # Formatting away from initial line
-puts "Hashword generator v 1.2 (build 20131126)" # Name and build version info (version info in yyyymmdd format)
+puts "Hashword generator v 2.0 (build 20141010)" # Name and build version info (version info in yyyymmdd format)
 puts "\n" # Formatting
 puts "Please enter your hashword seed." # Informs user to input hashword seed
 puts "*NOTE: hashword seeds are case sensitive.*" # Case-sensitivity notice
@@ -49,10 +49,10 @@ input = input.downcase # Reduces input to downcase
 
 ## 'If' statement for presenting long or short hashword
 if input == "yes" # Begins 'if yes' of boolean
-	long = Digest::SHA512.hexdigest(seed) # Defines 'long'
+	long = Digest::SHA256.hexdigest(seed) # Defines 'long'
 	puts "\n", "Your hashword is:", "\#" + long, "\n" # Prints defined long hashword
 elsif input == "no" # Begins 'if no' of boolean
-	short = Digest::SHA256.hexdigest(seed) # Defines 'short'
+	short = Digest::MD5.hexdigest(seed) # Defines 'short'
 	puts "\n", "Your hashword is:", "\#" + short, "\n" # Prints defined short hashword
 else # Defines escape if 'input' is neither 'yes' or 'no'
 	puts "Please answer Yes or No" # Suggests trying again and answering 'yes' or 'no'
