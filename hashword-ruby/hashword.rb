26dabb8d5add7575ee0e17ee511b9ea1ece04522
Joshua Embrey <mrschism@sdf.org>
2014-10-28 20:53:32 -0400

    Updated to follow newer convention
    Now 'short' is the default option.  Code optimization (because I was a shit coder last year).  Fixed minor formatting issues.
    
    Apparently it's a line longer, but it's more sane, too.... and works cross-platform.

--- hashword-ruby/hashword.rb
+++ hashword-ruby/hashword.rb
@@ -5,55 +5,56 @@
 # 	Joshua "MrSchism" Embrey [mrschism@sdf.org]
 # 	Joseph "Arcarna" Preston [jpreston86@gmail.com]
 # Intial commit: October 16, 2013
-# Current version: October 10, 2014
+# Current version: October 28, 2014
 #####################################################################################
 
 ## Declare required components
 require 'digest'
 require 'rbconfig'
 
+os = RbConfig::CONFIG['host_os']
+win = ['windows','mingw32', 'mingw64']
 ## Seed input
-system ("clear") # Starts fresh
+
+# Fix cross-platform clear function
+if win.include? os
+	system("cls")
+else
+	system ("clear")
+end
+
 puts "\n" # Formatting away from initial line
-puts "Hashword generator v 2.0 (build 20141010)" # Name and build version info (version info in yyyymmdd format)
+puts " Hashword generator v 2.0.2 (build 20141028)" # Name and build version info (version info in yyyymmdd format)
 puts "\n" # Formatting
-puts "Please enter your hashword seed." # Informs user to input hashword seed
-puts "*NOTE: hashword seeds are case sensitive.*" # Case-sensitivity notice
+puts " Please enter your hashword seed." # Informs user to input hashword seed
+puts " *NOTE: hashword seeds are case sensitive.*" # Case-sensitivity notice
 puts "\n" # Formatting
 
 # Strip echoing of user input
-if RbConfig::CONFIG['host_os'] == "windows"
+if win.include? os
 	system ("@echo off")
-elsif RbConfig::CONFIG['host_os'] == "linux-gnu"
-	system ("stty -echo")
 else 
 	system ("stty -echo")
 end
 
 # Get user input
-seed = [(print 'Seed: '), gets.chomp][1] # Input prompt for starting seed using single-line array
+seed = [(print ' Seed: '), gets.chomp][1] # Input prompt for starting seed using single-line array
 
 # Return echoing of user input
-if RbConfig::CONFIG['host_os'] == "windows"
+if win.include? os
 	system ("@echo on")
-elsif RbConfig::CONFIG['host_os'] == "linux-gnu"
-	system ("stty echo")
 else 
 	system ("stty echo")
 end
 
 ## Long mode boolean input
 puts "\n" # Formatting
-input = [(print 'Use Long mode? (yes/no): '), gets.chomp][1] # A chomped gets for input regarding long mode using single-line array
-input = input.downcase # Reduces input to downcase
+input = [(print ' Use Long mode? (yes/NO): '), gets.chomp][1] # A chomped gets for input regarding long mode using single-line array
+input = input.downcase
 
-## 'If' statement for presenting long or short hashword
-if input == "yes" # Begins 'if yes' of boolean
-	long = Digest::SHA256.hexdigest(seed) # Defines 'long'
-	puts "\n", "Your hashword is:", "\#" + long, "\n" # Prints defined long hashword
-elsif input == "no" # Begins 'if no' of boolean
-	short = Digest::MD5.hexdigest(seed) # Defines 'short'
-	puts "\n", "Your hashword is:", "\#" + short, "\n" # Prints defined short hashword
-else # Defines escape if 'input' is neither 'yes' or 'no'
-	puts "Please answer Yes or No" # Suggests trying again and answering 'yes' or 'no'
-end # Ends 'if' statement (and program).
+if ['yes', 'y'].include? input
+    hash = Digest::SHA256.hexdigest(seed)
+else
+    hash = Digest::MD5.hexdigest(seed)
+end
+puts "\n", " Your hashword is:", "  \#" + hash, "\n"
