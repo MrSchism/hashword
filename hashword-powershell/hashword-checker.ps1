b4242db44ab25c5fbc7658f0eeda305fe5fae5f8
Joshua Embrey <mrschism@sdf.org>
2015-04-16 19:46:58 -0400

    Hashword Checker v3
    This is the basis of the future of Hashword command line code.  I was
    going to make it incompatible with the initial hashword, but I'm not a
    douche.

--- hashword-powershell/hashword-checker.ps1
+++ hashword-powershell/hashword-checker.ps1
@@ -1,10 +1,15 @@
 ######################################################################################
-# hashword written by:
+# hashword-checker v3 written by:
 #         Joshua "MrSchism" Embrey [mrschism@sdf.org]
 #         Joseph "Arcarna" Preston [jpreston86@gmail.com]
-# Intial commit: November 4, 2014
-# Current version: November 4, 2014
+# Intial commit: April 16, 2015
+# Current version: April 16, 2015
 ######################################################################################
+
+# Params
+Param(
+    [switch]$legacy
+)
  
 # Algorithms
 $md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
@@ -12,14 +17,42 @@ $sha256 = new-object -TypeName System.Security.Cryptography.SHA256CryptoServiceP
  
 # Encoding
 $utf8 = new-object -TypeName System.Text.UTF8Encoding
- 
+
+# Used variables
+$alpha = 0
+$num = 1
+$dot = 3
+$dash = 5
+$at = 9
+
+# Function
+function myalgo($val){
+    $hashword = Switch($val -Split ''){
+        {$_ -match "\d" -and $num -ne $dot -and $num -ne $dash -and $num -ne $at}{$_;$num++;continue}
+        {$_ -match "\d" -and $num -eq $dot} {$_ + ".";$num++;continue}
+        {$_ -match "\d" -and $num -eq $dash} {$_ + "-";$num++;continue}
+        {$_ -match "\d" -and $num -eq $at} {$_ + "@";$num = 1;continue}
+        {$_ -match "[a-z]" -and $alpha -lt 3} {$_;$alpha++;continue}
+        {$_ -match "[a-z]"} {$alpha = 0;[char]::toupper($_)}
+    }
+    echo "#$($hashword -join '')"
+}
+
 echo "`n Seed?"
 $seed = read-host
  
 $short = ([System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($seed))) -replace "-",'').tolower()
 $long = ([System.BitConverter]::ToString($sha256.ComputeHash($utf8.GetBytes($seed))) -replace "-",'').tolower()
  
-echo "-----`n"
- 
-echo "Short:`n #$short `n"
-echo "Long:`n #$long `n"
+echo " -----`n"
+
+# Legacy mode
+if ($legacy -eq $true){ 
+    echo "Short:`n #$short `n"
+    echo "Long:`n #$long `n"
+}
+
+else {
+    echo "Short:`n $(myalgo $short) `n"
+    echo "Long:`n $(myalgo $long) `n"
+}
