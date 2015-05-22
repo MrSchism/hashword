272f5b2b35040943712d79e11589823e42cb28ec
Joshua Embrey <mrschism@sdf.org>
2015-05-22 14:57:17 -0400

    initial 3.0 update
    It's not done, but it's the initial v3 logic.
    
    I'll finish this later today

--- hashword-powershell/hashword.ps1
+++ hashword-powershell/hashword.ps1
@@ -1,22 +1,50 @@
 ######################################################################################
 # hashword written by:
-#	Joshua "MrSchism" Embrey [mrschism@sdf.org]
-#	Joseph "Arcarna" Preston [jpreston86@gmail.com]
+#  Joshua "MrSchism" Embrey [mrschism@sdf.org]
+#	 Joseph "Arcarna" Preston [jpreston86@gmail.com]
 # Intial commit: October 14, 2014
-# Current version: November 4, 2014
+# Current version: May 22, 2015
 ######################################################################################
- 
-# Define algorithms
+
+# Params
+Param(
+    [switch]$legacy
+)
+
+# Build
+$build = 20150522
+
+# Algorithms
 $md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
 $sha256 = new-object -TypeName System.Security.Cryptography.SHA256CryptoServiceProvider
  
 # Define encoding
 $utf8 = new-object -TypeName System.Text.UTF8Encoding
  
+# Used variables
+$alpha = 0
+$num = 1
+$dot = 3
+$dash = 5
+$at = 9
+
+# Function
+function myalgo($val){
+    $hashword = Switch($val -Split ''){
+        {$_ -match "\d" -and $num -ne $dot -and $num -ne $dash -and $num -ne $at}{$_;$num++;continue}
+        {$_ -match "\d" -and $num -eq $dot} {$_ + ".";$num++;continue}
+        {$_ -match "\d" -and $num -eq $dash} {$_ + "-";$num++;continue}
+        {$_ -match "\d" -and $num -eq $at} {$_ + "@";$num = 1;continue}
+        {$_ -match "[a-z]" -and $alpha -lt 3} {$_;$alpha++;continue}
+        {$_ -match "[a-z]"} {$alpha = 0;[char]::toupper($_)}
+    }
+    echo "#$($hashword -join '')"
+}
+
 #==== Actual script ====#
  
 cls
-echo "`n Hashword generator v 2.0 (build 20141104) `n"
+echo "`n Hashword generator v 3.0 (build $build) `n"
 echo " Please enter your hashword seed."
 echo " *NOTE: hashword seeds are case sensitive.*`n"
 $seed = read-host ' Seed' -AsSecure
